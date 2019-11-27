614dfbdb5d8621a825ea93507d363ebbee06514b
Ley Gabriel <gabriel.ley@outlook.fr>
2019-11-27 14:19:45 +0100

    heroku config

--- YLYL/YLYL/settings.py
+++ YLYL/YLYL/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -142,3 +144,5 @@ WEBPACK_LOADER = {
 
 LOGIN_REDIRECT_URL = '/game/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+django_heroku.settings(locals())
